eef23beea82359e490e28ef93eed6cf77f6d8170
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2026-04-11 10:11:26 +0800

    traffic light at start of road

--- one-way-road-simpy.py
+++ one-way-road-simpy.py
@@ -2,13 +2,31 @@ import simpy
 
 MAX_CARS = 5
 CAR_TIME = 2
+TRAFFIC_LIGHT_RED_TIME = 3
+# 0-2,6-8
+TRAFFIC_LIGHT_GREEN_TIME = 3
+
+
+def is_green(time):
+    return (time % (TRAFFIC_LIGHT_GREEN_TIME + TRAFFIC_LIGHT_RED_TIME)) in range(
+        TRAFFIC_LIGHT_GREEN_TIME
+    )
 
 
 # a car on a road
 def car(env, road, delay):
     while True:
-        print(f"Car parking at {env.now} for {delay} seconds")
-        yield env.timeout(delay)
+        if delay > 0:
+            print(f"Car parking at {env.now} for {delay} seconds")
+            yield env.timeout(delay)
+
+        if not is_green(env.now):
+            print("Waiting for traffic light to turn green...")
+        while not is_green(env.now):
+            yield env.timeout(1)
+
+        print("Traffic light is green, moving forward")
+
         with road.request() as req:
             yield req
             print(f"Car driving at start of intersection at {env.now}")
@@ -21,5 +39,5 @@ road = simpy.Resource(env, capacity=MAX_CARS)
 car_gen = car(env, road, 0)
 car_gen_delay = car(env, road, 1)
 simpy.Process(env=env, generator=car_gen)
-simpy.Process(env=env, generator=car_gen_delay)
-env.run(until=50)
+# simpy.Process(env=env, generator=car_gen_delay)
+env.run(until=15)
